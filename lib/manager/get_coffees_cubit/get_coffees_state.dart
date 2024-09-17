a948aec45ca5dd887983891574170acf4bce82da
shiba <just.shiba3006@gmail.com>
2024-09-17 21:52:22 +0300

    add get coffees states

--- lib/manager/get_coffees_cubit/get_coffees_state.dart
+++ lib/manager/get_coffees_cubit/get_coffees_state.dart
@@ -4,3 +4,15 @@ part of 'get_coffees_cubit.dart';
 sealed class GetCoffeesState {}
 
 final class GetCoffeesInitial extends GetCoffeesState {}
+
+final class GetCoffeesLoadingState extends GetCoffeesState {}
+
+final class GetCoffeesSuccessState extends GetCoffeesState {
+  final List<CoffeeModel> coffees;
+  GetCoffeesSuccessState({required this.coffees});
+}
+
+final class GetCoffeesFailureState extends GetCoffeesState {
+  final String message;
+  GetCoffeesFailureState({required this.message});
+}
