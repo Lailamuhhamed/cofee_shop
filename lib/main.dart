338317c928a25043ce45c801a16b4a0914960e82
shiba <just.shiba3006@gmail.com>
2024-09-17 22:18:12 +0300

    init my bloc observer in main file

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'my_bloc_observer.dart';
 import 'views/coffee_home.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // init bloc observer
+  Bloc.observer = MyBlocObserver();
   runApp(const CoffeeApp());
 }
 
