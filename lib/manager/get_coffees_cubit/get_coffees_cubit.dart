396b0f58c3aa3bcd2f98ca11f22c2b7abc46c171
shiba <just.shiba3006@gmail.com>
2024-09-17 21:54:05 +0300

    add get coffees cubit impls

--- lib/manager/get_coffees_cubit/get_coffees_cubit.dart
+++ lib/manager/get_coffees_cubit/get_coffees_cubit.dart
@@ -1,8 +1,18 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
+import '../../api/api_service.dart';
+import '../../models/coffee_model.dart';
+
 part 'get_coffees_state.dart';
 
 class GetCoffeesCubit extends Cubit<GetCoffeesState> {
   GetCoffeesCubit() : super(GetCoffeesInitial());
+
+  List<CoffeeModel> coffees = [];
+  Future<void> getCoffees() async {
+    emit(GetCoffeesLoadingState());
+
+    emit(GetCoffeesSuccessState(coffees: coffees));
+  }
 }
