6e30b336a4fe291f1bc31ae195a5a4568f7a8da6
shiba <just.shiba3006@gmail.com>
2024-09-17 22:17:29 +0300

    add my bloc observer file

---
+++ lib/my_bloc_observer.dart
@@ -0,0 +1,12 @@
+// create my bloc observer
+import 'dart:developer';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MyBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    log('${bloc.runtimeType} $change');
+  }
+}
